b21a1539e5e891f068257f8dbdd43ec842d510d7
Jayanth Srinivasan <jayanthsrinivasan2003@gmail.com>
2024-09-21 17:32:33 +0530

    Refactor DAO contributor dashboard page

--- app/dao/[daoId]/page.tsx
+++ app/dao/[daoId]/page.tsx
@@ -7,82 +7,169 @@ import { useRouter } from "next/navigation";
 const ContributorDashboard = () => {
   const router = useRouter();
 
+  const redirectToPage = (page: string) => {
+    router.push(page);
+  };
+
   return (
-    <main className="min-h-screen bg-gray-900 text-white font-mont">
+    <main className="min-h-screen tracking-widest text-white font-lond">
       <section className="p-4 md:px-16 lg:max-w-5xl lg:mx-auto py-[50px] md:py-[80px]">
         <div className="text-center mb-10">
           <h2 className="text-3xl lg:text-5xl font-bold">
             Contributor Dashboard
           </h2>
+          <p className="text-gray-400 mt-2 text-lg">
+            Select one of the options to proceed.
+          </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Tasks Overview */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+          {/* Active Tasks */}
+          <div
+            className="bg-gray-800 p-6 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition-all"
+            onClick={() => redirectToPage("/dao/tasks")}
+          >
+            <div className="card-icon mb-4 flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12 text-emerald-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z"
+                />
+              </svg>
+            </div>
             <h3 className="text-2xl font-bold mb-4">Active Tasks</h3>
             <p className="text-gray-400 mb-4">
               View tasks and bounties available for contribution.
             </p>
-            <Button
-              className="bg-emerald-500 text-white w-full"
-              onClick={() => router.push("/dao/contributor/tasks")}
-            >
+            <Button className="bg-emerald-500 text-white w-full">
               View Tasks
             </Button>
           </div>
 
-          {/* Polls Overview */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+          {/* Active Polls */}
+          <div
+            className="bg-gray-800 p-6 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition-all"
+            onClick={() => redirectToPage("/dao/polls")}
+          >
+            <div className="card-icon mb-4 flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12 text-emerald-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002"
+                />
+              </svg>
+            </div>
             <h3 className="text-2xl font-bold mb-4">Active Polls</h3>
             <p className="text-gray-400 mb-4">
               Participate in DAO governance by voting on proposals.
             </p>
-            <Button
-              className="bg-emerald-500 text-white w-full"
-              onClick={() => router.push("/dao/contributor/polls")}
-            >
+            <Button className="bg-emerald-500 text-white w-full">
               View Polls
             </Button>
           </div>
 
-          {/* Events Overview */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+          {/* Upcoming Events */}
+          <div
+            className="bg-gray-800 p-6 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition-all"
+            onClick={() => redirectToPage("/dao/events")}
+          >
+            <div className="card-icon mb-4 flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12 text-emerald-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+                />
+              </svg>
+            </div>
             <h3 className="text-2xl font-bold mb-4">Upcoming Events</h3>
             <p className="text-gray-400 mb-4">
               Join online events, IRL events, or participate in giveaways.
             </p>
-            <Button
-              className="bg-emerald-500 text-white w-full"
-              onClick={() => router.push("/dao/contributor/events")}
-            >
+            <Button className="bg-emerald-500 text-white w-full">
               View Events
             </Button>
           </div>
 
-          {/* Marketplace Overview */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+          {/* Marketplace */}
+          <div
+            className="bg-gray-800 p-6 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition-all"
+            onClick={() => redirectToPage("/dao/marketplace")}
+          >
+            <div className="card-icon mb-4 flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12 text-emerald-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                />
+              </svg>
+            </div>
             <h3 className="text-2xl font-bold mb-4">Marketplace</h3>
             <p className="text-gray-400 mb-4">
               Browse and purchase NFTs, merchandise, and more.
             </p>
-            <Button
-              className="bg-emerald-500 text-white w-full"
-              onClick={() => router.push("/dao/contributor/marketplace")}
-            >
+            <Button className="bg-emerald-500 text-white w-full">
               View Marketplace
             </Button>
           </div>
 
-          {/* Community Chat Overview */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+          {/* Community Chat */}
+          <div
+            className="bg-gray-800 p-6 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition-all"
+            onClick={() => redirectToPage("/dao/community")}
+          >
+            <div className="card-icon mb-4 flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12 text-emerald-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </div>
             <h3 className="text-2xl font-bold mb-4">Community Chat</h3>
             <p className="text-gray-400 mb-4">
               Join discussions and interact with other contributors.
             </p>
-            <Button
-              className="bg-emerald-500 text-white w-full"
-              onClick={() => router.push("/dao/contributor/community")}
-            >
+            <Button className="bg-emerald-500 text-white w-full">
               Join Chat
             </Button>
           </div>
